599952633f0775d83f84310b80b6fc3a3a7dde22
pechera <romankotliar7@gmail.com>
2023-03-05 21:47:27 +0200

    main

--- config/nodemailer.config.js
+++ config/nodemailer.config.js
@@ -1,11 +1,11 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
+  host: process.env.MAIL_HOST,
   port: 587,
   auth: {
-    user: "virgil.morar@ethereal.email",
-    pass: "BB7etH8Kgmm7K7vaUY",
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
 });
 
